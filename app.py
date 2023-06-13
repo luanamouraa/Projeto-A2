16a811158aaabe0d5a64292603fd7d555aa16568
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-06-13 19:03:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import pandas as pd
 
 df = pd.read_excel('Reviews_iphone.xlsx')
 st.write(df)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
