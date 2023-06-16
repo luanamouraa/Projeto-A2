33b9d1666bc4a0a18d0052973f0da3d9399dbe18
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-06-16 08:04:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,19 +22,13 @@ from vega_datasets import data
 
 source = data.cars()
 
-chart = alt.Chart(source).mark_circle().encode(
-    x='sentiment',
-    y='contagem'
-).interactive()
+chart = alt.Chart(source).mark_circle().encode(x='sentiment', y='contagem').interactive()
 
 tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
 
 with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
 with tab2:
-    # Use the native Altair theme.
     st.altair_chart(chart, theme=None, use_container_width=True)
 
 
